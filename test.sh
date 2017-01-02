a424041510d777975cecea94435279afda391f15
Henrique Bastos <henrique@bastos.net>
2017-01-02 03:12:39 -0200

    Add a primitive test suite.

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# A first primitive golden file test.
+
+diff data/sample.beancount <(gnucash-to-beancount data/sample.gnucash) &&
+diff data/sample-bean-report-accounts.txt <(bean-report data/sample.beancount accounts) &&
+diff data/sample-bean-report-balances.txt <(bean-report data/sample.beancount balances) &&
+diff data/sample-bean-report-stats-postings.txt <(bean-report data/sample.beancount stats-postings) &&
+diff data/sample-bean-report-stats-types.txt <(bean-report data/sample.beancount stats-types)
